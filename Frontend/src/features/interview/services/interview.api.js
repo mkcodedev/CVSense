b3aa99543963590d9f838a6405153ff8a554471a
mkcodedev <mohitsaraswat108@gmail.com>
2026-08-03 18:59:17 +0530

    backend url fixing

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "http://localhost:3000, https://cvsense-qwv2.onrender.com",
     withCredentials: true,
 })
 

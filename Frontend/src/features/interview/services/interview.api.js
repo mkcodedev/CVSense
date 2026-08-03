9140b9304851baa62d743703d1def5cc7e1f39aa
mkcodedev <mohitsaraswat108@gmail.com>
2026-08-03 19:20:14 +0530

    bug fixing

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,9 +1,4 @@
-import axios from "axios";
-
-const api = axios.create({
-    baseURL: "http://localhost:3000, https://cvsense-qwv2.onrender.com",
-    withCredentials: true,
-})
+import { api } from "../../../lib/api"
 
 
 /**
